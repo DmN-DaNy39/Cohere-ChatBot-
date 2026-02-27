c7bb13e7d7f27d81699314ade178edffc1137bab
DmN-DaNy39 <148595802+DmN-DaNy39@users.noreply.github.com>
2026-02-27 23:00:25 +0000

    first commit

---
+++ cohereVA.py
@@ -0,0 +1,57 @@
+import speech_recognition as sr
+import pyttsx3
+import cohere
+#initialising cohere API key
+co = cohere.ClientV2("ohFOgxRJse8JieBvehy34x8YcUqQ8KKu1PAt6ozl")
+
+#Send prompt to cohere through API 
+def chat(prompt):
+    response = co.chat(
+        model="command-a-03-2025",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+    )
+
+    reply = response.message.content[0].text.strip()
+    tts(reply)
+
+#Listen to the user and return what the user said
+def speechRecog():
+    recognizer = sr.Recognizer()
+    try:
+        with sr.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = recognizer.listen(mic)
+
+            text = recognizer.recognize_google(audio)
+            text = text.lower()
+
+            print(f"User: {text}")
+            return text
+
+    except sr.UnknownValueError:
+        return ""
+
+#creating the text to speech engine to say the ai response
+def tts(text):
+    print(f"CPU: {text}")
+    engine = pyttsx3.init(driverName='sapi5')
+    engine.setProperty('rate', 150)
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
+    del engine
+
+
+while True:
+    user_input = speechRecog()
+    if user_input.lower() in ["quit", "exit", "bye"]:
+        break
+    else:
+        chat(user_input)
+
+    
